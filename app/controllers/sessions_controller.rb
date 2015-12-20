58d8ad16b3d03ca3cb863a3626c7b6ca7ab9c13c
Kate Wood <katewood611@gmail.com>
2015-12-19 18:22:02 -0800

    first commit

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,22 @@
+class SessionsController < ApplicationController
+
+	def new
+	end
+
+	def create
+		user = User.find_by(email: params[:login][:email])
+		if user && user.authenticate(params[:login][:password])
+			session[:user_id] = user.id.to_s
+			redirect_to root_path
+		else
+			render :new
+			# @error = "Email and password do not match"
+		end
+	end
+
+	def destroy
+		session.delete(:user_id)
+		redirect_to login_path
+	end
+
+end
